10c44543ca2b39609cfb45e5df7928353af2d2dd
KJ Hassel <khassel@uncc.edu>
2019-09-03 14:34:00 +0000

    RubyBasics1 –Part 1

---
+++ RubyBasics1/lib/rubybasics1.rb
@@ -0,0 +1,18 @@
+# Lab 1
+# Part I
+def sum arr
+  # YOUR CODE HERE
+  sum = 0;
+  arr.each { |x| sum+=x }
+ return sum;
+end
+
+# Part II
+def max_2_sum arr
+  # YOUR CODE HERE
+end
+
+# Part III
+def sum_to_n? arr, n
+  # YOUR CODE HERE
+end
