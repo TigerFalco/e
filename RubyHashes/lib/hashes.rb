aa9fdb6f7e70ae9347fa609e84f4d441b0778342
KJ Hassel <khassel@uncc.edu>
2019-09-06 01:38:06 +0000

    RubyHashes – Part 3

--- RubyHashes/lib/hashes.rb
+++ RubyHashes/lib/hashes.rb
@@ -17,7 +17,17 @@ end
 
 # Part II
 def array2d_2_hash contact_info, contacts
-    # YOUR CODE HERE
+   index = 0
+    contacts.each do |contact_info, _unused|
+        contacts = contact_info[index]
+        if contact_info == nil
+            contacts[contact_info] = ""
+        else
+            contacts[contact_info] = contact_info
+        end
+        index += 1
+    end
+    return contacts
 end
 
 # Part III
